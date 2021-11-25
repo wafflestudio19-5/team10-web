3e76abe7bfdc449f622d07cf55b5621025656bac
andhl <andhlwhffu@gmail.com>
2021-11-25 21:48:27 +0900

    docs css 문서들

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import TrackPage from "./components/ArtistPage/Track/TrackPage";
+import Header from "./components/Header/Header";
+import styles from "./App.module.scss";
+import Discover from "./components/Discover/Discover";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className={styles.App}>
+      <Header />
+      <TrackPage />
+    </div>
+  );
 }
 
 export default App;
