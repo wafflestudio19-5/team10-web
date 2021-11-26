78fa689d9644b25020dc111c14b4a68b8b0b301a
Arsture <87017787+Arsture@users.noreply.github.com>
2021-11-26 17:23:20 +0900

    feat router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import Login from "./components/Login/Login";
+import TrackPage from "./components/ArtistPage/Track/TrackPage";
+import Header from "./components/Header/Header";
 import Discover from "./components/Discover/Discover";
-import styles from "./App.module.scss";
+import ArtistPage from "./components/ArtistPage/ArtistPage";
+import Logout from "./components/Logout/Logout";
+import { Switch, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
   return (
-    <div className={styles.app}>
-      <Discover />
+    <div className="app">
+      {location.pathname !== "/" ? <Header /> : null}
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/discover" component={Discover} />
+        <Route exact path="/you/library" component={ArtistPage} />
+        <Route exact path="/username/trackname" component={TrackPage} />
+        <Route exact path="/logout" component={Logout} />
+      </Switch>
     </div>
   );
 }
