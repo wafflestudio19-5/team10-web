e481269746f6e13499fd5dc41a80137353cc14c0
andhl <andhlwhffu@gmail.com>
2021-12-19 12:35:51 +0900

    docs conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,41 @@ import ArtistPage from "./components/ArtistPage/ArtistPage";
 import Logout from "./components/Logout/Logout";
 import { Switch, Route, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import YourTracks from "./components/ArtistPage/Track/YourTracks/YourTracks";
+import Library from "./components/Library/Library";
+import styles from "./App.module.scss";
+import Likes from "./components/Library/Likes/Likes";
+import Playlists from "./components/Library/Playlists/Playlists";
+import Albums from "./components/Library/Albums/Albums";
+import Stations from "./components/Library/Stations/Stations";
+import Following from "./components/Library/Following/Following";
+import History from "./components/Library/History/History";
+import { AuthProvider } from "./Context";
+import Upload from "./components/Upload/Upload";
 
 function App() {
   const location = useLocation();
   return (
-    <div>
-      <Toaster />
-      {location.pathname !== "/" ? <Header /> : null}
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/discover" component={Discover} />
-        <Route exact path="/username" component={ArtistPage} />
-        <Route exact path="/username/trackname" component={TrackPage} />
-        <Route exact path="/logout" component={Logout} />
-        <Route exact path="/you/tracks" component={YourTracks} />
-      </Switch>
-    </div>
+    <AuthProvider>
+      <div className={styles.wrapper}>
+        <Toaster />
+        {location.pathname !== "/" ? <Header /> : null}
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/discover" component={Discover} />
+          <Route exact path="/username" component={ArtistPage} />
+          <Route exact path="/username/trackname" component={TrackPage} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/you/library" component={Library} />
+          <Route exact path="/you/likes" component={Likes} />
+          <Route exact path="/you/sets" component={Playlists} />
+          <Route exact path="/you/albums" component={Albums} />
+          <Route exact path="/you/stations" component={Stations} />
+          <Route exact path="/you/following" component={Following} />
+          <Route exact path="/you/history" component={History} />
+          <Route exact path="/upload" component={Upload} />
+        </Switch>
+      </div>
+    </AuthProvider>
   );
 }
 
