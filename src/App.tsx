873566dd185f101b9ff28762f60389788be095dd
hmlinnie <88392435+hmlinnie@users.noreply.github.com>
2022-01-28 11:43:55 +0900

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -21,10 +21,6 @@ import SetPage from "./components/ArtistPage/Set/SetPage";
 import ArtistPageTracks from "./components/ArtistPage/ArtistPageTracks/ArtistPageTracks";
 import ArtistPagePlaylists from "./components/ArtistPage/ArtistPagePlaylists/ArtistPagePlaylists";
 import History from "./components/Library/History/History";
-// import ArtistPageReposts from "./components/ArtistPage/ArtistPageReposts/ArtistPageReposts";
-// import ArtistPageRepostsTracks from "./components/ArtistPage/ArtistPageReposts/ArtistPageRepostsTracks/ArtistPageRepostsTracks";
-// import ArtistPageRepostsSets from "./components/ArtistPage/ArtistPageReposts/ArtistPageRepostsSets/ArtistPageRepostsSets";
-// import ArtistPageAlbums from "./components/ArtistPage/ArtistPageAlbums/ArtistPageAlbums";
 
 function App() {
   const location = useLocation();
@@ -60,29 +56,11 @@ function App() {
               <ArtistPageTracks key={props.match.params.permalink} {...props} />
             )}
           />
-          {/* <Route exact path="/:permalink/albums" component={ArtistPageAlbums} /> */}
           <Route
             exact
             path="/:permalink/sets"
             component={ArtistPagePlaylists}
           />
-          {/* <Route
-            exact
-            path="/:permalink/reposts"
-            component={ArtistPageReposts}
-          />
-          <Route
-            exact
-            path="/:permalink/reposts/tracks"
-            component={ArtistPageRepostsTracks}
-          />
-          <Route
-            exact
-            path="/:permalink/reposts/sets"
-            component={ArtistPageRepostsSets}
-          />
-          <Route exact path="/:username/:trackname" component={TrackPage} />
-          <Route exact path="/:username/sets/:playlist" component={SetPage} /> */}
         </Switch>
         {location.pathname !== "/" && <TrackBar />}
         {location.pathname !== "/" && <AudioTag />}
